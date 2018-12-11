7aa95eec9669098e80430eb0b0321dfc05b5419a
James Dawson <jamesdawsonWD@gmail.com>
2018-12-11 13:53:49 +0000

    add: new router modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { SvgJamesComponent } from './shared/svg-james/svg-james.component';
-import { CodeReviewComponent } from './components/code-review/code-review.component';
 import { CodeTestComponent } from './components/code-test/code-test.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     SvgJamesComponent,
-    CodeReviewComponent,
     CodeTestComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
