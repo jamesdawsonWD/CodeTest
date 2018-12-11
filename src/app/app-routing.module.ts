be0717113875d5b294bb2e9c8806e29f5f411447
James Dawson <jamesdawsonWD@gmail.com>
2018-12-11 13:54:28 +0000

    chore: remove app-routing for code review

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { CodeTestComponent } from './components/code-test/code-test.component';
-import { CodeReviewComponent } from './components/code-review/code-review.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent},
   { path: 'code-test', component: CodeTestComponent },
-  { path: 'code-review', component: CodeReviewComponent },
   { path: '**', redirectTo: '' },
 ];
 
