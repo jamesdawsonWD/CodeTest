b3de81efa255c2ee81cc996522038c524e342d71
James Dawson <jamesdawsonWD@gmail.com>
2018-12-11 13:54:48 +0000

    remove: code-review component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,46 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, inject } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Router} from '@angular/router';
+class MockRouter {
+  navigateByUrl(url: string) { return url; }
+}
 describe('AppComponent', () => {
+  let fixture;
+  let app;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
       ],
-    }).compileComponents();
-  }));
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: Router,
+          useClass: MockRouter
+        }
+      ]
+    }).compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
+      });
+    }
+  ));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('superAwesomeCodeTest');
+  it(`should have as title 'Super Awesome Code Test'`, async(() => {
+
+    expect(app.title).toEqual('Super Awesome Code Test');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should have a router outlet', async(() => {
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to superAwesomeCodeTest!');
+    app = fixture.debugElement.nativeElement;
+    expect(app.querySelector('router-outlet')).toBeTruthy();
   }));
+
 });
